a0aa242015a686214b47e3d6d98773b1130e911d
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-07-19 19:36:39 -0300

    Add files via upload

---
+++ Aula 10/aula10_script.js
@@ -0,0 +1,17 @@
+var a = window.document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar() {
+    a.innerText = 'Clicou!'
+    a.style.background = 'Blue'
+}
+function entrar() {
+    a.innerText = 'Entrou!'
+    a.style.background = 'Black'
+}
+function sair() {
+    a.innerText = "Saiu!"
+    a.style.background = 'Red'
+}

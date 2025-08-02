9cfffb6857bb857d0fd66dd52617b0e3a565833c
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-08-01 21:51:23 -0300

    Add files via upload

---
+++ Módulo E/Aula 13/Aula 14/ambiente2.js
@@ -0,0 +1,19 @@
+//while
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//do while
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+    
+//for
+for (var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}

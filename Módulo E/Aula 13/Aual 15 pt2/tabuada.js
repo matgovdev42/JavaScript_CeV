b09cdcd5cc1e38d91ebf8136b9cbf18f5dbb48fc
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-08-06 15:31:40 -0300

    Update tabuada.js

--- Módulo E/Aula 13/Aual 15 pt2/tabuada.js
+++ Módulo E/Aula 13/Aual 15 pt2/tabuada.js
@@ -8,12 +8,13 @@ function calcular(){
         let c = 1
         tab.innerHTML = ''
         while (c <= 10){
-            let item = document.createElement('opition')
+            let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
     }
+
 
 }
